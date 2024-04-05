218104b2fb295c84741ef70ea11fe3930c8e7745
valeriely7 <valeriely@hotmail.com>
2024-04-05 04:46:22 -0600

    css and html

--- js/script.js
+++ js/script.js
@@ -1,99 +1,29 @@
-const API_URL = `https://api.thecatapi.com/v1/`;
-const API_KEY = "DEMO-API-KEY";
+const url = "https://api.thecatapi.com/v1/images/search"
 
-let currentImageToVoteOn;
+const section = document.querySelector('.container');
+const button = document.querySelector ('.btn');
 
-function showHistoricVotes()
-{
-  
-  document.getElementById('vote-options').style.display = 'none';
-  document.getElementById('vote-results').style.display = 'block';
+button.addEventListener('click', getRandomCats);
 
-  const url = `${API_URL}votes?limit=10&order=DESC`;
+randomCatPhoto = (json) => {
+    let photo = json[0].url
+    selection.classList.add('cats')
 
-  fetch(url,{headers: {
-    'x-api-key': API_KEY
-  }})
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-  
-    data.map(function(voteData) {
- 
-    const imageData = voteData.image
- 
-    let image = document.createElement('img');
-     //use the url from the image object
-     image.src = imageData.url
-            
-    let gridCell = document.createElement('div');
-    
-      if(voteData.value<0)
-      {
-        gridCell.classList.add('red') 
-      } else {
-        gridCell.classList.add('green')
-      }
-      
-    gridCell.classList.add('col-lg');
+    let image = document.createElement ('img')
+    image.src = photo;
+    image.classList.add('random_cats')
+    image.alt = photo
+    section.appendChild(image);
+};
 
-    gridCell.appendChild(image)
-       
-    document.getElementById('grid').appendChild(gridCell);
-       
-    });
-  
-  })
-  .catch(function(error) {
-     console.log(error);
-  });
-  
+async function getRandomCats() {
+    section.innerHTML = ""
+    try{
+        const reposnse = await fetch(url)
+        const json = await reposnse.json()
+        console.log('JSON:', json);
+        return randomCatPhoto(json)
+    } catch {
+
+    }
 }
-
-function showVoteOptions()
-{
-  document.getElementById("grid").innerHTML = '';
-  
-  document.getElementById('vote-options').style.display = 'block';
-  document.getElementById('vote-results').style.display = 'none';
-  
-  showImageToVoteOn()
-}
-
-function showImageToVoteOn()
-{
-  
-  const url = `${API_URL}images/search`;
-
-  fetch(url,{headers: {
-    'x-api-key': API_KEY
-  }})
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    currentImageToVoteOn = data[0];
-    document.getElementById("image-to-vote-on").src= currentImageToVoteOn.url;
-  });
-
-}
-
-function vote(value)
-{
-  
-  const url = `${API_URL}votes/`;
-  const body = {
-    image_id:currentImageToVoteOn.id,
-    value
-  }
-  fetch(url,{method:"POST",body:JSON.stringify(body),headers: {
-    'content-type':"application/json",
-    'x-api-key': API_KEY
-  }})
-  .then((response) => {
-    showVoteOptions()
-  })
-}
-
-showVoteOptions()
